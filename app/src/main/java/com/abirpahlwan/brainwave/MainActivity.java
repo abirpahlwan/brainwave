ed3588262e3022a16545db6a48a21ce59472ea3c
abirpahwlan <abir978@gmail.com>
2023-07-02 13:02:20 +0600

    init project

---
+++ app/src/main/java/com/abirpahlwan/brainwave/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abirpahlwan.brainwave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
